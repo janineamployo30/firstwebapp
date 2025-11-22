e99cc99f200f77115a6a454c44454c091f4bc291
janineamployo30 <22-68670@g.batstate-u.edu.ph>
2025-11-22 07:46:17 +0000

    Update message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.write("Name: Janine C. Amployo\n");
     res.write("Section: BSIT BA-4102\n");
     res.write("My Life Verse: 1 Corinthians 10:31 NIV\n");
-    res.write("[31] So whether you eat or drink or whatever you do, do it all for the glory of God."\n);
+    res.write("[31] So whether you eat or drink or whatever you do, do it all for the glory of God.\n");
     res.write("\nTo God be all the glory!");
     res.end();
 });
