cc8363f88bb140b9324b29f6cedcdd8ca0a09fe4
janineamployo30 <22-68670@g.batstate-u.edu.ph>
2025-11-22 06:51:53 +0000

    Add package.json

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.write("Janine C. Amployo\n");
+    res.write("BSIT BA-4102\n");
+    res.write("1 Corinthians 10:31 NIV\n");
+    res.write("31 So whether you eat or drink or whatever you do, do it all for the glory of God.");
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
